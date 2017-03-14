2dcf34823a14ee71beb7efd5bffb2bb592b256cd
Charles <yaoxin04@58ganji.com>
2017-03-14 14:51:42 +0800

    Initial Commit

---
+++ NET/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NET
+//
+//  Created by Charles on 2017/3/14.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NET/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NET
+//
+//  Created by Charles on 2017/3/14.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
